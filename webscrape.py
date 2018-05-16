ca37efaeb70bef870833bf81d94288148e3d2f03
Sangjin <smithsk013@gmail.com>
2018-05-16 09:23:55 -0400

    initial commit

---
+++ webscrape.py
@@ -0,0 +1,8 @@
+# Import all the dependencies
+import pandas as pd
+import requests
+import json
+import pprint
+from splinter import Browser
+from splinter.exceptions import ElementDoesNotExist
+from bs4 import BeautifulSoup
